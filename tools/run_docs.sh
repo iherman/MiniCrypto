2302e3c03be5182be6a8f5d67f4721abe7b01dc7
Ivan Herman <ivan@ivan-herman.net>
2024-12-04 11:40:13 +0100

    All missing docs files

---
+++ tools/run_docs.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+# shellcheck disable=SC2164
+(cd docs; /bin/rm -rf *)
+cp index.ts multikey_webcrypto.ts
+deno run -A tools/copy_readme.ts
+deno doc --html --name="Mini Crypto API" index.ts lib/*
+mv multikey_webcrypto.ts index.ts
+(cd docs; touch .nojekyll)
